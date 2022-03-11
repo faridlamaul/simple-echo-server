19fa13d3f81704a08e3b49f8a822a1b0456c9181
faridlamaul <lamaulfarid71@gmail.com>
2022-03-12 02:41:54 +0700

    add simple echo server single & multiple

---
+++ src/MultipleClient.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Inet4Address;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MultipleClient {
+    public static void main(String[] args) throws Exception {
+        try {
+            Inet4Address addr = (Inet4Address) Inet4Address.getLoopbackAddress();
+            ServerSocket server = new ServerSocket(6666, 5, addr);
+            while(true) {
+                System.out.println("1 - Server started");
+                Socket client = server.accept();
+                System.out.println("2 - Client connected");
+                
+                BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                System.out.println("3 - BufferedReader and BufferedWriter created");
+                System.out.println("4 - Waiting for client to send data");
+    
+                // read the message from client (readline lebih baik karena text-based protocol ex. HTTP)
+                String message = br.readLine();
+                System.out.println("5 - Message received");
+                // print the message
+                System.out.println("Message from client: " + message);
+    
+                bw.write(message);
+                bw.flush();
+                // write the message to client
+                // close the connection
+    
+                client.close();
+            }
+            // server.close();
+        } catch (Exception e) {
+            Logger.getLogger(MultipleClient.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+}
